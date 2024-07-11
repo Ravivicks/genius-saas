9d9672a1978ee8f605a42d7fb5e3bcc4df6a072d
Ravi Anuragi <anuragiravi85@gmail.com>
2024-07-11 12:58:54 +0530

    Remove .env file from repository

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,30 +1,7 @@
-"use client";
+import React from "react";
 
-import { useChat } from "ai/react";
+const Landing = () => {
+  return <div>Landing</div>;
+};
 
-export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat({
-    api: "/api/chat",
-  });
-
-  return (
-    <div>
-      <ul>
-        {messages.map((m, index) => (
-          <li key={index}>
-            {m.role === "user" ? "User: " : "AI: "}
-            {m.content}
-          </li>
-        ))}
-      </ul>
-
-      <form onSubmit={handleSubmit}>
-        <label>
-          Say something...
-          <input value={input} onChange={handleInputChange} />
-        </label>
-        <button type="submit">Send</button>
-      </form>
-    </div>
-  );
-}
+export default Landing;
